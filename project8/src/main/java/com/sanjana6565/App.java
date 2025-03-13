b1c41ae15e3bb23b5622df1136d3b36d305681c5
sanjana6565 <sanjanasomashekar6@gmail.com>
2025-03-13 15:04:31 +0530

    testa

---
+++ project8/src/main/java/com/sanjana6565/App.java
@@ -0,0 +1,13 @@
+package com.sanjana6565;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
